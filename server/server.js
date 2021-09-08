cde05c88ea920b434e476fc0befa887a6f063f55
Rudy Sanchez <rudys212@gmail.com>
2021-09-08 16:08:12 -0700

    urlencoded to true

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ server.start().then(() => {
   server.applyMiddleware({ app });
 });
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
