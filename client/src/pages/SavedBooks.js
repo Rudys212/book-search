3ac3498ca7bb833ca245e0d20668729122b2e3d8
Rudy Sanchez <rudys212@gmail.com>
2021-09-08 02:51:32 -0700

    completed using mutations

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -11,6 +11,7 @@ import Auth from "../utils/auth";
 import { removeBookId } from "../utils/localStorage";
 import { GET_ME } from "../utils/queries";
 import { REMOVE_BOOK } from "../utils/mutations";
+import { useQuery, useMutation } from "@apollo/client";
 
 const SavedBooks = () => {
   const [userData, setUserData] = useState({});
